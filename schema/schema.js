47adde84246e9b2f6a00671ce37b62b39a93466a
Ricky Drohan <rdrohanirl@yahoo.ie>
2018-07-07 19:31:13 +0100

    Added single country call with list of leagues

--- schema/schema.js
+++ schema/schema.js
@@ -1,23 +1,46 @@
 const graphql = require('graphql');
 
 const Country = require('../models/country');
+const League = require('../models/league');
 
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
+  GraphQLInt,
   GraphQLList
 } = graphql;
 
 const CountryType = new GraphQLObjectType({
-    name: 'Country',
-    fields: () => ({
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      trigramme: { type: GraphQLString }
-    })
-  });
+  name: 'Country',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    trigramme: { type: GraphQLString },
+    leagues: {
+      type: new GraphQLList(LeagueType),
+      resolve(parent, args) {
+        return League.find({ countryId: parent.id });
+      }
+    }
+  })
+});
+
+const LeagueType = new GraphQLObjectType({
+  name: 'League',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    level: { type: GraphQLInt },
+    country: {
+      type: CountryType,
+      resolve(parent, args) {
+        return Country.findById(parent.countryId);
+      }
+    }
+  })
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -26,6 +49,13 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(CountryType),
       resolve(parent, args) { 
         return Country.find({});
+      }
+    },
+    country: {
+      type: CountryType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) { 
+        return Country.findById(args.id);
       }
     }
   }
