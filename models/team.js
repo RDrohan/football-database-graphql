e43e06b3a2171d4e197bf216a5d56e33e4e2a06d
Ricky Drohan <rdrohanirl@yahoo.ie>
2018-07-08 19:51:54 +0100

    Added League schema and query with list of Teams

---
+++ models/team.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const teamSchema = new Schema({
+  name: String,
+  yearFounded: Number,
+  stadiumId: String,
+  leagueId: String
+});
+
+module.exports = mongoose.model('Team', teamSchema, 'team');
