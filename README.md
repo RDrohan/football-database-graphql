a897c8868253001db700f02a4432114db35afddd
Ricky Drohan <rdrohanirl@yahoo.ie>
2018-11-26 23:00:25 +0000

    Added competition and winner data for Northern Ireland and updated README.md

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Football database created using Mongoose and GraphQL
 3. Run `npm install` while in root directory
 4. Run `npm run seeder` to create the database on your computer
 5. Run `npm start` to run database server
-6. Access site by going to your [browser](http://localhost:4000/graphql)
+6. Access site by going to your [browser](http://localhost:4000/graphql) or alternatively use [my football app](https://github.com/RDrohan/football-database-app)!
--- data/competition.js
+++ data/competition.js
@@ -4,30 +4,15 @@ const ObjectId = require('mongoose').Types.ObjectId;
 module.exports = {
 	name: Competition,
 	data: [
-        {
-            _id : ObjectId('5b43bddaf0ed995f6a5e13a7'),
-            name : 'FAI Cup',
-            countryId : '5b3fa73f13591ee3884458a2'
-        },
-        {
-            _id : ObjectId('5b43bddaf0ed995f6a5e13a8'),
-            name : 'FAI President\'s Cup',
-            countryId : '5b3fa73f13591ee3884458a2'
-        },
-        {
-            _id : ObjectId('5b43bddaf0ed995f6a5e13a9'),
-            name : 'FAI League Cup',
-            countryId : '5b3fa73f13591ee3884458a2'
-        },
-        {
-            _id : ObjectId('5b43bddaf0ed995f6a5e13aa'),
-            name : 'FAI First Division',
-            countryId : '5b3fa73f13591ee3884458a2'
-        },
-        {
-            _id : ObjectId('5b43bddaf0ed995f6a5e13ab'),
-            name : 'FAI Premier Division',
-            countryId : '5b3fa73f13591ee3884458a2'
-        }
+        { _id : ObjectId('5b43bddaf0ed995f6a5e13a7'), name: 'FAI Cup', countryId: '5b3fa73f13591ee3884458a2' },
+        { _id : ObjectId('5b43bddaf0ed995f6a5e13a8'), name: 'FAI President\'s Cup', countryId: '5b3fa73f13591ee3884458a2' },
+        { _id : ObjectId('5b43bddaf0ed995f6a5e13a9'), name: 'FAI League Cup', countryId: '5b3fa73f13591ee3884458a2' },
+        { _id : ObjectId('5b43bddaf0ed995f6a5e13aa'), name: 'FAI First Division', countryId: '5b3fa73f13591ee3884458a2' },
+        { _id : ObjectId('5b43bddaf0ed995f6a5e13ab'), name: 'FAI Premier Division', countryId: '5b3fa73f13591ee3884458a2' },
+        { _id : ObjectId('5bfc6c6e49b8d59b04cef2fa'), name: 'Danske Bank Premiership', countryId: '5b3fa73f13591ee3884458a3' },
+        { _id : ObjectId('5bfc6ca449b8d59b04cef306'), name: 'NIFL Bluefin Sport Championship', countryId: '5b3fa73f13591ee3884458a3' },
+        { _id : ObjectId('5bfc6cbe49b8d59b04cef30d'), name: 'NIFL Premier Intermediate League', countryId: '5b3fa73f13591ee3884458a3' },
+        { _id : ObjectId('5bfc6ce249b8d59b04cef314'), name: 'Tennant\'s Irish Cup', countryId: '5b3fa73f13591ee3884458a3' },
+        { _id : ObjectId('5bfc6d1049b8d59b04cef31c'), name: 'BetMcClean League Cup', countryId: '5b3fa73f13591ee3884458a3' }
 	]
 }
